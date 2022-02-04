c5af8406034f31b79b01e3107e67a34873bea381
akaminer11 <akaminer11@gmail.com>
2022-02-04 11:57:55 -0500

    Added invalid syntax error handling.

--- translation.py
+++ translation.py
@@ -1,6 +1,21 @@
 #!/bin/python
+import sys
+
+def invalid_syntax():
+    with open("help.txt") as helpfile:
+        h = helpfile.readlines()
+        print("#####################################################\nFatal error: Invalid syntax. Printing help statement.\n#####################################################\n")
+        for line in h:
+            line = line[:-1]
+            print(line, sep='')
+        print("\n\n")
 
 
+def take_input():
+    args = sys.argv
+    if len(args) > 3:
+        invalid_syntax()
+        break
 
 
 def parse(phrase):
@@ -13,17 +28,18 @@ def parse(phrase):
     langs = words[1:]
     langs[2] = langs[2][:-1]
     return eng, langs
-    
+   
 
 def main():
     with open("words.txt", "r") as w:
         lines = w.readlines()
-
     words = {}
-
     for phrase in lines:
         eng, langs = parse(phrase)
         words[eng] = langs
+
+    take_input()
+
 
 
 if __name__ == "__main__":
