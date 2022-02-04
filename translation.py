d178d14a0f849268aea2ce904039c5fefe5c12be
akaminer11 <akaminer11@gmail.com>
2022-02-04 12:34:48 -0500

    Finished main functionality, fixed input, added good error messaging, and did a little bit of refactoring.

--- translation.py
+++ translation.py
@@ -1,21 +1,69 @@
 #!/bin/python
 import sys
 
-def invalid_syntax():
-    with open("help.txt") as helpfile:
+
+def print_help():
+    with open("help.txt", "r") as helpfile:
         h = helpfile.readlines()
-        print("#####################################################\nFatal error: Invalid syntax. Printing help statement.\n#####################################################\n")
-        for line in h:
-            line = line[:-1]
-            print(line, sep='')
-        print("\n\n")
+    for line in h:
+        line = line[:-1]
+        print(line, sep='')
+    print("\n\n")
 
 
-def take_input():
+
+def invalid_syntax():
+    print("#####################################################\nFatal error: Invalid syntax. Printing help statement.\n#####################################################\n")
+    print_help()
+
+
+def invalid_lang():
+    print("##########################################\nFatal error: Invalid language. Printing help statement.\n#######################################\n")
+    print_help()
+
+
+def word_not_found():
+    print("##############################\nError: Word not found in wordlist. Printing help statement.\n##########################################")
+    print_help()
+
+
+
+def take_input(words):
+    acceptable_helps = ["-h", "--help"]
+    acceptable_langs = ["-s", "-f", "-g", "--german", "--french", "--spanish"]
+    german = ["-g", "--german"]
+    french = ["-f", "--french"]
+    spanish = ["-s", "--spanish"]
     args = sys.argv
-    if len(args) > 3:
+    if len(args) == 2:
+        h = args[1]
+        if h not in acceptable_helps:
+            invalid_syntax()
+        else:
+            print_help()
+    elif len(args) == 3:
+        lang = args[1]
+        word = args[2]
+        if lang not in acceptable_langs:
+            invalid_lang()
+        else:
+            if lang in german:
+                lang_num = 2
+            elif lang in french:
+                lang_num = 1
+            else:
+                lang_num = 0
+            translate(word, words, lang_num)
+    else:
         invalid_syntax()
-        return -1
+
+
+
+def translate(word, words, lang):
+    if word in words:
+        print(words[word][lang])
+    else:
+        word_not_found()
 
 
 def parse(phrase):
@@ -30,6 +78,7 @@ def parse(phrase):
     return eng, langs
    
 
+
 def main():
     with open("words.txt", "r") as w:
         lines = w.readlines()
@@ -37,11 +86,8 @@ def main():
     for phrase in lines:
         eng, langs = parse(phrase)
         words[eng] = langs
-
-    word = take_input()
-    if word == -1:
-        return
-
+    take_input(words)
+    
 
 
 if __name__ == "__main__":
